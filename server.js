9c89ec2801a3a3eb60e3b9cc9d7bdd728aa3313f
D-Livermore <d.livermore907@mybvc.ca>
2025-03-13 05:32:53 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-const express = require('express');
 const dotenv = require('dotenv');
+const express = require('express');
 const { Sequelize } = require('sequelize');
 const cors = require('cors');  
 const bodyParser = require('body-parser');
